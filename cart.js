d82e95fc8aadfee9847e44811124d3fa652c96d1
ADS 2023.2N <36129382023.2N@senacrjedu.local>
2023-11-29 21:26:57 -0300

    remover e add item

--- cart.js
+++ cart.js
@@ -6,9 +6,28 @@ function adicionarItem(item) {
     var qtde = document.getElementById('quantidade' + item)
     var total = document.getElementById('total' + item)
     qtd[item] +=1
-    valorTotal[0] = Number.parseFloat(valorProduto[item]* qtd[item])
+    valorTotal[item] = Number.parseFloat(valorProduto[item]* qtd[item])
     qtde.innerHTML = qtd[item]
-    total.innerHTML = valorTotal[item].toFixed(2)
-    
-    
+    total.innerHTML = valorTotal[item].toFixed(2)    
 }
+
+function removerItem(item) {
+    if(qtd[item] > 0) {
+        qtd[item] -= 1;
+        var qtde = document.getElementById('quantidade'+item)
+        var total = document.getElementById('total'+item)
+        valorTotal[item] = Number.parseFloat(valorProduto[item] * qtd[item]);
+        qtde.innerHTML = qtd[item];
+        total.innerHTML = valorTotal[item].toFixed(2)
+    }
+}
+
+function valorCompra() {
+    var valorTotalCompra = document.getElementById('valorTotalCompra');
+    var valor = 0
+    for (var i = 0; i < valorTotal.length; i++) {
+        valor += valorTotal[i]
+    }
+    valorTotalCompra.innerHTML = valor.toFixed(2)
+}
+
